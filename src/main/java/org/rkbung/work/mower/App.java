3b0f8d8f69b78781a360bb0cde27991126acddb8
rkbung <rkbung@gmail.com>
2013-02-16 20:11:39 +0100

    init

---
+++ src/main/java/org/rkbung/work/mower/App.java
@@ -0,0 +1,13 @@
+package org.rkbung.work.mower;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
