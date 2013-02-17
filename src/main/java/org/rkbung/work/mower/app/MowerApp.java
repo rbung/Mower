93cf59e5b16d4f21b7ecc510a1bf256f2b22f770
rkbung <rkbung@gmail.com>
2013-02-17 23:24:02 +0100

    implementing app

--- src/main/java/org/rkbung/work/mower/app/MowerApp.java
+++ src/main/java/org/rkbung/work/mower/app/MowerApp.java
@@ -7,7 +7,7 @@ import org.rkbung.work.mower.service.impl.MowerService;
 import java.io.File;
 
 public class MowerApp {
-    IMowerService mowerService;
+    private IMowerService mowerService;
 
     public MowerApp() {
         mowerService = new MowerService();
@@ -15,13 +15,20 @@ public class MowerApp {
 
     public static void main(String[] args) {
         if(ArrayUtils.getLength(args) != 1) {
-            System.out.println("Usage :  java MowerApp PATH_TO_MOWERS_FILE");
+            usage();
             System.exit(-1);
         }
         File mowersFile = new File(args[0]);
         if(!mowersFile.exists()){
             System.out.println("File does not exist !");
+            usage();
             System.exit(-1);
+        } else {
+            System.out.println("File is " + mowersFile.getAbsolutePath());
         }
+    }
+
+    private static void usage() {
+        System.out.println("Usage :  java MowerApp PATH_TO_MOWERS_FILE");
     }
 }
